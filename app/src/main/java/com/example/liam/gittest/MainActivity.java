949c238e78bec1b50ac99ae0285f7b23af4af197
LiamLong <565436441@qq.com>
2016-10-23 17:23:43 +0800

    第一次修改

--- app/src/main/java/com/example/liam/gittest/MainActivity.java
+++ app/src/main/java/com/example/liam/gittest/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.liam.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "hahaah";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, "onCreate: " );
     }
 }
